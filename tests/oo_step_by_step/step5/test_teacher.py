6734cc568ef41751027d582516a61435d1c46d55
Songbai Yan <colorguitar@hotmail.com>
2019-12-24 21:58:33 +0800

    remove useless stdout code

--- tests/oo_step_by_step/step5/test_teacher.py
+++ tests/oo_step_by_step/step5/test_teacher.py
@@ -9,22 +9,9 @@ from oo_step_by_step.step5.teacher import Teacher
 from oo_step_by_step.step5.x import X
 
 
-def stub_stdout(testcase_inst):
-    stderr = sys.stderr
-    stdout = sys.stdout
-
-    def cleanup():
-        sys.stderr = stderr
-        sys.stdout = stdout
-
-    testcase_inst.addCleanup(cleanup)
-    sys.stderr = io.StringIO()
-    sys.stdout = io.StringIO()
-
-
 class TestTeacher(unittest.TestCase):
     def test_teacher_say_student_joined_class(self):
-        stub_stdout(self)
+        sys.stdout = io.StringIO()
         student = Student("1", "Tom", 18, Klass(1))
         klass = Klass(2)
         teacher = Teacher("1", "Jerry", 18, [Klass(2)])
@@ -33,7 +20,7 @@ class TestTeacher(unittest.TestCase):
         self.assertEqual(str(sys.stdout.getvalue()), 'I am Jerry. I know Tom has joined Class 2.\n')
 
     def test_teacher_say_student_became_leader(self):
-        stub_stdout(self)
+        sys.stdout = io.StringIO()
         klass = Klass(1)
         student = Student("1", "Tom", 18, klass)
         teacher = Teacher("1", "Jerry", 18, [klass])
@@ -42,7 +29,7 @@ class TestTeacher(unittest.TestCase):
         self.assertEqual(str(sys.stdout.getvalue()), 'I am Jerry. I know Tom become Leader of Class 1.\n')
 
     def test_teacher_and_x_say_student_joined_class(self):
-        stub_stdout(self)
+        sys.stdout = io.StringIO()
         student = Student("1", "Tom", 18, Klass(1))
         klass = Klass(2)
         teacher = Teacher("1", "Jerry", 18, [Klass(2)])
@@ -54,7 +41,7 @@ class TestTeacher(unittest.TestCase):
                          'I am Jerry. I know Tom has joined Class 2.\nI am the Machine. I know Tom has joined Class 2.\n')
 
     def test_teacher_and_x_say_student_became_leader(self):
-        stub_stdout(self)
+        sys.stdout = io.StringIO()
         klass = Klass(1)
         student = Student("1", "Tom", 18, klass)
         teacher = Teacher("1", "Jerry", 18, [klass])
